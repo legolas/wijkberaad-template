b483fe03bd9c23b3b2b130b3f03020a3263352e9
Marcel <legolasgreenlf@gmail.com>
2012-11-23 13:24:48 +0100

    Added files

---
+++ src/main/web/html/mod_footer/default.php
@@ -0,0 +1,5 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+<small><?php echo $lineone; ?> Developed by
+<a href="http://endofdullart.nl/" title="Bezoek End Of Dull Art" target="blank">endofdullart.nl</a>.</small>
+<!--<small><?php echo JText::_( 'FOOTER_LINE2' ); ?></small> -->
